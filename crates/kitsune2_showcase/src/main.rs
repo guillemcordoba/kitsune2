ba60bbb7b5322eebfa5128e15be98553d933a7d8
guillem.cordoba <guillem.cordoba@gmail.com>
2025-09-03 12:14:52 +0200

    Fix logs

--- crates/kitsune2_showcase/src/main.rs
+++ crates/kitsune2_showcase/src/main.rs
@@ -26,12 +26,12 @@ struct Args {
 #[tokio::main]
 async fn main() {
     let pid = std::process::id();
-    let file_appender = tracing_appender::rolling::never(
-        "./logs",
-        format!("{}-{}.log", env!("CARGO_PKG_NAME"), pid),
-    );
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-    tracing_subscriber::fmt().with_writer(non_blocking).init();
+    // let file_appender = tracing_appender::rolling::never(
+    //     "./logs",
+    //     format!("{}-{}.log", env!("CARGO_PKG_NAME"), pid),
+    // );
+    // let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    // tracing_subscriber::fmt().with_writer(non_blocking).init();
 
     let _ = rustls::crypto::aws_lc_rs::default_provider().install_default();
 
